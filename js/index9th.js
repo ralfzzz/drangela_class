cb1a34fa71676e86aa300227b951c847640fda64
ralfzzz <azbimanyu@gmail.com>
2023-06-11 17:35:40 +0700

    Update index9th.js

--- js/index9th.js
+++ js/index9th.js
@@ -10,3 +10,21 @@ function whosPaying(names) {
     return output;
 /******Don't change the code below*******/    
 }
+
+var nBottles = 99;
+function fizzBuzz() {
+
+    while (nBottles>0){
+    if (nBottles > 1) {
+          console.log(nBottles + " bottles of beer on the wall. Take one down, pass it around,"); 
+          nBottles--;
+          console.log(nBottles + " bottles of beer on the wall");  
+    } else if (nBottles = 1){
+          console.log(nBottles + " bottle of beer on the wall. Take one down, pass it around,");
+          nBottles--;
+          console.log("No bottles of beer on the wall");  
+    }
+    }
+    nBottles = 99;
+    console.log("No bottles of beer on the wall, Get " + nBottles + " again!");
+}
