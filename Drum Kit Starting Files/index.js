25cb0fabf79cac7de11c913a9d3de87ff58f65d0
ralfzzz <azbimanyu@gmail.com>
2023-06-12 20:35:37 +0700

    update drum

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("wwwwwwwwwwwwww");
+    });    
+}
+
