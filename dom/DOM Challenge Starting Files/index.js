0670e849a485d150ee374d9ab09ca8874fe1aaec
ralfzzz <azbimanyu@gmail.com>
2023-06-12 16:49:02 +0700

    update js & css

--- dom/DOM Challenge Starting Files/index.js
+++ dom/DOM Challenge Starting Files/index.js
@@ -1,1 +1,11 @@
 document.query
+
+// document.querySelector("li a").style.color=("white");
+
+// var third = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild; 
+// third.innerHTML = "Rifqi A";
+
+// document.querySelector(".btn").style.backgroundColor = "yellow";
+
+// document.querySelector("h1").classList.toggle("huge");
+
