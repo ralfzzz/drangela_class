e10de766cf7ac695029ec4a7758cdb04d697a785
ralfzzz <azbimanyu@gmail.com>
2023-06-13 11:37:06 +0700

    update js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,18 @@
 
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("wwwwwwwwwwwwww");
-    });    
-}
+// for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+//     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+//         alert("wwwwwwwwwwwwww");
+//     });    
+// }
 
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+        document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+            
+        });    
+    }
+
+
+    // var audio = new Audio('./sounds/tom-1.mp3');
+    //         audio.play();
+    // this.style.color="red";
--- js/index11th.js
+++ js/index11th.js
@@ -13,3 +13,28 @@ function divide(num1,num2) {
 function calculator(num1,num2,operator) {
     return operator(num1,num2);
 }
+
+//evenlistener
+$0.innerHTML="haha";
+'haha'
+$0.addEventListener("click", function () {
+    alert("hahah");
+});
+
+
+//object
+var houseKeeper1 = {
+    nama : "hahi",
+    age : 22,
+    languages : [
+        "indo",
+        "eng"
+    ]
+}
+
+//constractor function
+function HouseKeeper(nama,age,languages) {
+    this.nama = nama;
+    this.age = age;
+    this.languages = languages;
+}
