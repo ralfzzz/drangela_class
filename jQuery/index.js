eab3ad786298f3c646086752bc8a27f1e79ac5a1
ralfzzz <azbimanyu@gmail.com>
2023-06-17 19:50:50 +0700

    Update index.js

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,2 +1,6 @@
-$("h1").css("color", "red");
-$("*").css("background-color", "white");
+// $("h1").css("color", "red");
+// $("*").css("background-color", "white");
+
+$("h1").text("ai");
+$("button").text("aihhu");
+$($("button")[1]).html("<strong>hah</strong>"); //selecting one
