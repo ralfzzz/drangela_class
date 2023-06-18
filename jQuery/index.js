0a78c15ca2c39ec4b2809ca7efd23b15c0e3fe56
ralfzzz <azbimanyu@gmail.com>
2023-06-18 19:05:09 +0700

    update jquery files

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,6 +1,23 @@
 // $("h1").css("color", "red");
 // $("*").css("background-color", "white");
 
-$("h1").text("ai");
-$("button").text("aihhu");
-$($("button")[1]).html("<strong>hah</strong>"); //selecting one
+// $("h1").text("ai");
+// $("button").text("aihhu");
+// $($("button")[1]).html("<strong>hah</strong>"); //selecting one
+
+// $("button").click(()=> {
+//     $("h1").css("color","red");
+// });
+
+// $("input").keypress((event)=> {
+//     console.log(event.key);
+// });
+
+// $(document).keypress((event)=> {
+//     $("h1").text(event.key);
+// });
+
+$("button").click(()=> {
+    $("h1").slideUp().slideDown().animate({opacity: '0.3'});
+});
+
