c87c79c495ebd52dbab3ccb572929ff03a7e6dc6
ralfzzz <azbimanyu@gmail.com>
2023-06-21 20:26:51 +0700

    Update game.js

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -25,10 +25,8 @@ function animateFlash(randomChoosenColour) {
 }
 
 function palySounds(randomChoosenColour) {
-
     var audio = new Audio("./sounds/"+randomChoosenColour+".mp3");
     audio.play();
-
 }
 
 function animatePress(currentColor) {
@@ -56,8 +54,8 @@ function startOver() {
     gamePattern = [];
 }
 
+//logic game
 function checkAnswer(currentLevel) {
-
         if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
             console.log("true");
             if (gamePattern.length === userClickedPattern.length) {
@@ -75,13 +73,6 @@ function checkAnswer(currentLevel) {
             startOver();
         }
 }
-
-// function arrayEquals(a, b) {
-//     return Array.isArray(a) &&
-//         Array.isArray(b) &&
-//         a.length === b.length &&
-//         a.every((val, index) => val === b[index]);
-// }
 
 //click pattern
 $(".btn").click((event) => {
@@ -113,27 +104,34 @@ $(".btn-start").click(() => {
 
 
 // var key = randomChoosenColour;
-    // switch (key) {
-    //     case "red":
-    //         var audioRed = new Audio('./sounds/red.mp3');
-    //         audioRed.play();
-    //         break;
+// switch (key) {
+//     case "red":
+//         var audioRed = new Audio('./sounds/red.mp3');
+//         audioRed.play();
+//         break;
+
+//     case "blue":
+//         var audioRed = new Audio('./sounds/blue.mp3');
+//         audioRed.play();
+//         break;
+
+//     case "green":
+//         var audioRed = new Audio('./sounds/green.mp3');
+//         audioRed.play();
+//         break;
+
+//     case "yellow":
+//         var audioRed = new Audio('./sounds/yellow.mp3');
+//         audioRed.play();
+//         break;
     
-    //     case "blue":
-    //         var audioRed = new Audio('./sounds/blue.mp3');
-    //         audioRed.play();
-    //         break;
+//     default:console.log(key);
+//         break;
+// }
 
-    //     case "green":
-    //         var audioRed = new Audio('./sounds/green.mp3');
-    //         audioRed.play();
-    //         break;
-
-    //     case "yellow":
-    //         var audioRed = new Audio('./sounds/yellow.mp3');
-    //         audioRed.play();
-    //         break;
-        
-    //     default:console.log(key);
-    //         break;
-    // }
+// function arrayEquals(a, b) {
+//     return Array.isArray(a) &&
+//         Array.isArray(b) &&
+//         a.length === b.length &&
+//         a.every((val, index) => val === b[index]);
+// }
