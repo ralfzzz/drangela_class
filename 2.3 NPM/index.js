255e08581188f8c5261beed5246437202d6c75df
ralfzzz <azbimanyu@gmail.com>
2023-06-23 15:48:39 +0700

    update npm

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// import generateName from "happn-sillyname"; ecmascript modul (ecm)
+// var generateName from ('happn-sillyname'); common js (cjs)
+// var sillyName = generateName();
+// const superheroes = require('superheroes');
+import superheroes from "superheroes";
+const heroName = superheroes.random();
+
+
+// console.log('Your name is: ${heroName}!');
+// console.log(`I am ${heroName}!`);
+console.log(`hi ${heroName}`);
+// console.log("I am ${superheroes}");
