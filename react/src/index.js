05eb3dda60e7c35396e0099f09be06f909bf092c
ralfzzz <azbimanyu@gmail.com>
2023-12-13 20:35:25 +0700

    initial push react subject class

---
+++ react/src/index.js
@@ -0,0 +1,10 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello Worl!d</h1>
+    <p>TEsting testing!</p>
+  </div>,
+  document.getElementById("root")
+);
