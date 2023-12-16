ba19bce0b8dae4ad535961f46fef750aa51aed09
ralfzzz <azbimanyu@gmail.com>
2023-12-16 07:47:08 +0700

    Update App.jsx

--- emojipedia/src/components/App.jsx
+++ emojipedia/src/components/App.jsx
@@ -2,11 +2,18 @@ import React from "react";
 const {Card} = require('./Card');
 const {emojipedia} = require('../emojipedia');
 
-function generateCard(emojiData){
-    return(
-        <Card key={emojiData.id} emoji={emojiData.emoji} name={emojiData.name} meaning={emojiData.meaning}/>
-    )
-}
+// function generateCard(emojiData){
+//     return(
+//         <Card key={emojiData.id} emoji={emojiData.emoji} name={emojiData.name} meaning={emojiData.meaning.substring(0,100)}/>
+//     )
+// }
+
+var test = emojipedia.find(({id})=> id===4);
+// console.log(test);
+
+var test2 = emojipedia.map((value)=>{
+    return value.meaning.substring(0,100);
+})
 
 function App() {
   return (
@@ -16,7 +23,11 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-       {emojipedia.map(generateCard)}
+       {emojipedia.map((emojiData)=>{
+        return(
+        <Card key={emojiData.id} emoji={emojiData.emoji} name={emojiData.name} meaning={emojiData.meaning.substring(0,100)}/>
+    )
+       })}
       </dl>
     </div>
   );
