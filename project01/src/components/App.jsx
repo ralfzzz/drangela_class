522190d6bc91255c43396c62c380ae52455b9431
ralfzzz <azbimanyu@gmail.com>
2023-12-18 16:59:26 +0700

    Update App.jsx
    try form logic in react

--- project01/src/components/App.jsx
+++ project01/src/components/App.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function App() {
 
   const [button, setButton] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [submitValue, setSubmitValue] = useState("");
 
   function changeButtonBlack(){
     setButton("black");
@@ -11,12 +13,25 @@ function App() {
   function changeButtonNormal(){
     setButton("");
   }
+  
+  function getInputValue(event){
+    setInputValue(event.target.value);
+  }
+
+  function submitInputValue(event){
+    setSubmitValue(inputValue);
+
+    event.preventDefault();
+  }
+
 
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button onMouseEnter={changeButtonBlack} onMouseLeave={changeButtonNormal} className={button}>Submit</button>
+      <h1>Hello {submitValue}</h1>
+    <form onSubmit={submitInputValue}>
+      <input onChange={getInputValue} type="text" placeholder="What's your name?" value={inputValue}/>
+      <button onClick={submitInputValue} onMouseEnter={changeButtonBlack} onMouseLeave={changeButtonNormal} className={button}type="submit" >Submit</button>
+    </form>
     </div>
   );
 }
