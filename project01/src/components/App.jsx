abe7046d2bebc9140ebc41b3d31ff55fffd3efda
ralfzzz <azbimanyu@gmail.com>
2023-12-19 14:30:37 +0700

    Update App.jsx
    changing complex state react

--- project01/src/components/App.jsx
+++ project01/src/components/App.jsx
@@ -3,8 +3,15 @@ import React, { useState } from "react";
 function App() {
 
   const [button, setButton] = useState("");
-  const [inputValue, setInputValue] = useState("");
-  const [submitValue, setSubmitValue] = useState("");
+  const [fullNameValue, setFullNameValue] = useState({
+    firstName:"",
+    lastName:""
+  });
+
+  // const [inputValue, setInputValue] = useState("");
+  // const [submitValue, setSubmitValue] = useState("");
+  // const [fNameValue, setFNameValue] = useState("");
+  // const [lNameValue, setLNameValue] = useState("");
 
   function changeButtonBlack(){
     setButton("black");
@@ -13,24 +20,60 @@ function App() {
   function changeButtonNormal(){
     setButton("");
   }
+
+  function getFullNameValue(event){
+    // const inputName = event.target.name;
+    // const inputValue = event.target.value;
+
+    const {name,value} = event.target;
+
+    setFullNameValue((prev)=>{
+      if (name === "firstName") {
+        return({
+          firstName:value,
+          lastName:prev.lastName
+        })
+      } else if (name === "lastName") {
+        return({
+          firstName:prev.firstName,
+          lastName:value
+        })
+      }
+    });
+    // console.log(inputName);
+    // console.log(inputValue);
+  }
   
-  function getInputValue(event){
-    setInputValue(event.target.value);
-  }
+  // function getInputValue(event){
+  //   setInputValue(event.target.value);
+  // }
+  
+  // function getFNameValue(event){
+  //   setFNameValue(event.target.value);
+  // }
 
-  function submitInputValue(event){
-    setSubmitValue(inputValue);
+  // function getLNameValue(event){
+  //   setLNameValue(event.target.value);
+  // }
 
-    event.preventDefault();
-  }
+  // function submitInputValue(event){
+  //   setSubmitValue(inputValue);
+
+  //   event.preventDefault();
+  // }
 
 
   return (
     <div className="container">
-      <h1>Hello {submitValue}</h1>
-    <form onSubmit={submitInputValue}>
-      <input onChange={getInputValue} type="text" placeholder="What's your name?" value={inputValue}/>
-      <button onClick={submitInputValue} onMouseEnter={changeButtonBlack} onMouseLeave={changeButtonNormal} className={button}type="submit" >Submit</button>
+      <h1>Hello {fullNameValue.firstName} {fullNameValue.lastName}</h1>
+    <form onSubmit="/">
+      <input onChange={getFullNameValue} name="firstName" type="text" placeholder="What's your fName?" value={fullNameValue.firstName}/>
+      <input onChange={getFullNameValue} name="lastName" type="text" placeholder="What's your lName?" value={fullNameValue.lastName}/>
+      {/* <input onChange={getFNameValue} type="text" placeholder="What's your fName?" value={fNameValue}/> */}
+      {/* <input onChange={getLNameValue} type="text" placeholder="What's your lName?" value={lNameValue}/> */}
+      {/* <input onChange={getInputValue} type="text" placeholder="What's your lName?" value={inputValue}/> */}
+      {/* <button onClick={submitInputValue} onMouseEnter={changeButtonBlack} onMouseLeave={changeButtonNormal} className={button}type="submit" >Submit</button> */}
+      <button onMouseEnter={changeButtonBlack} onMouseLeave={changeButtonNormal} className={button}type="submit" >Submit</button>
     </form>
     </div>
   );
