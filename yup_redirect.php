7fd23f2d105d0c2c952b0a74721e9b070d772bbe
ralfzzz <azbimanyu@gmail.com>
2024-04-09 09:49:34 +0700

    delete

--- yup_redirect.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (preg_match('/iPhone|iPod|iPad/', $_SERVER['HTTP_USER_AGENT'])) {
-    header("Location: https://app.appsflyer.com/id1571716640?pid=telkomsel_int&c=Telk0408&af_siteid=G19&af_click_lookback=7d&clickid=<<MSISDNMASKING>>", true, 302);
-    exit();
-} else {
-    header("Location: https://app.appsflyer.com/com.yup.bank.digital.kartu.kredit.debit.pascabayar.pinjaman.investasi?pid=telkomsel_int&c=Telk0408&af_siteid=G19&af_click_lookback=7d&clickid=<<MSISDNMASKING>>", true, 302);
-    exit();
-}
